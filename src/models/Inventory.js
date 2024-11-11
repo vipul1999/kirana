ab190e14dce8b50e86a16a9ea271830eeb4ad56f
vipulkrishna <vipulkrishna.mathuria@delhivery.com>
2024-11-11 22:34:52 +0530

    updated schema

--- src/models/Inventory.js
+++ src/models/Inventory.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const inventorySchema = new mongoose.Schema({
     name: String,
     quantity: Number,
-    price: Number
+    price: Number,
+    category: String,
+    shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'Shop' }
   });
   
 const Inventory = mongoose.model('Inventory', inventorySchema);
